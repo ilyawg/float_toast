db8ce3d6b545bb2a8582efd6c22165593c6688a5
ilyawg <ilyawg@mail.ru>
2021-11-12 15:45:25 +0300

    update to api level 30

--- app/src/main/java/com/kalmar/floattoast/CityViewHolder.kt
+++ app/src/main/java/com/kalmar/floattoast/CityViewHolder.kt
@@ -2,6 +2,7 @@ package com.kalmar.floattoast
 
 import android.util.Log
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -19,11 +20,19 @@ class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val toastButton:Button=itemView.findViewById(R.id.toast_button)
         toastButton.setOnClickListener{
-            val toast = Toast.makeText(itemView.context, city.population.toString(), Toast.LENGTH_SHORT)
+//            val toast = Toast.makeText(itemView.context, city.population.toString(), Toast.LENGTH_SHORT)
+
+            val toastView: View = LayoutInflater.from(itemView.context).inflate(R.layout.custom_toast,null)
+            val toastTextView: TextView = toastView.findViewById(R.id.toast_text)
+            toastTextView.text = city.population.toString()
+            val toast = Toast(itemView.context)
+            toast.view = toastView
+
             val location :IntArray=IntArray(2)
-            toastButton.getLocationOnScreen(location)
-//            Log.d("DEBUG_MSG", location[1].toString())
-            toast.setGravity(Gravity.TOP,0,location[1])
+            toastButton.getLocationInWindow(location)
+            val location1 = IntArray(2)
+//           Log.d("DEBUG_MSG", location[1].toString())
+            toast.setGravity(Gravity.TOP,0,location[1]-135)
             toast.show()
         }
     }
